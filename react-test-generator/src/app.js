579fe58f996b0a0b80f12362d7bcb6150dc9f69d
potapovDim <dmytrop@weblium.com>
2017-04-29 16:33:38 +0300

    initial commit , add deps

---
+++ react-test-generator/src/app.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Main extends Component {
+  render() {
+    return(
+      <div>
+        MAIN COMPONENT
+      </div>
+    )
+  }
+}
+
+export default Main
---
+++ react-test-generator/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+
+module.exports = {
+
+  entry: path.resolve('./src/index.js'),
+  output: {
+    path: path.resolve('./'),
+    filename: "index.js"
+  },
+  devServer: {
+    port: 9090,
+    historyApiFallback: true
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+        }
+      }
+    ]
+  }
+}
